9447545b6d17449ee7c17c7fa4b5c997ec7e7fee
blurdel <5648737+blurdel@users.noreply.github.com>
2022-08-20 16:45:12 -0400

    initial commit

---
+++ src/main/java/com/blurdel/web/thymeleaf/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.blurdel.web.thymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+
+		model.addAttribute("message", "Thank you for visiting.");
+		return "index";
+	}
+	
+}
---
+++ src/main/java/com/blurdel/web/thymeleaf/controllers/WelcomeController.java
@@ -0,0 +1,30 @@
+package com.blurdel.web.thymeleaf.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+
+	@Value("${welcome.message}")
+	private String message;
+	
+	private List<String> tasks = Arrays.asList("one", "two", "three", "four", "five");
+	
+	@GetMapping("/hello")
+	public String main(
+			@RequestParam(name = "name", required = false, defaultValue = "") String name, 
+			Model model) 
+	{
+		model.addAttribute("message", name.length() > 0 ? name : message);
+		model.addAttribute("tasks", tasks);
+		return "welcome";		
+	}
+	
+}
